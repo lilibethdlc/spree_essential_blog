376096a938ed18f51f600111b3bab82dc0518027
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-03-25 19:53:30 -0400

    Adding drop down to select author for blog posts

--- app/controllers/blog/admin/posts_controller.rb
+++ app/controllers/blog/admin/posts_controller.rb
@@ -1,27 +1,32 @@
 class Blog::Admin::PostsController < Admin::ResourceController
-  
+    before_filter :get_authors, :only => [:new, :edit]
+
   private
-    
+
     update.before :set_category_ids
-    
+
+    def get_authors
+        @authors = ::Instructor.all
+    end
+
     def set_category_ids
       if params[:post] && params[:post][:post_category_ids].is_a?(Array)
         params[:post][:post_category_ids].reject!{|i| i.to_i == 0 }
       end
     end
-    
+
     def translated_object_name
       I18n.t('post.model_name')
     end
-    
+
     def location_after_save
       params[:redirect_to] || object_url
-    end 
-    
+    end
+
     def find_resource
 	  	@object ||= Post.find_by_path(params[:id])
     end
-    
+
     def collection
       params[:search] ||= {}
       params[:search][:meta_sort] ||= "posted_at.desc"
